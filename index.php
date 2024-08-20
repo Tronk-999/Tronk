6d31d09ddd93e04dd9aa4f69a45f0b622dd71988
Tronk <trongnguyen123.123.123.123.4@gmail.com>
2024-08-20 12:52:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<head>
+    <link rel="shortcut icon" type="image.png" href="h2.jpg" />
+    <title>chat</title>
+    <style>
+    body {
+      background-image: url('hel.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: 100% 100%;
+    }
+    </style>
+    <style>
+        @keyframes buttonAnimation {
+          0% { transform: scale(1); }
+          50% { transform: scale(1.2); }
+          200% { transform: scale(1.2.4); }
+        }
+        .hbutton {
+          transition-duration: 0.4s;
+        }
+
+        .button:hover {
+          background-color: #04AA6D; /* Green */
+          color: white;
+        }
+        .button {
+          border: true;
+          cursor: pointer;
+          animation: buttonAnimation 2s infinite;
+        }
+        .column {
+  float: left;
+  width: 33.33%;
+  padding: 15px;
+        }
+        .container {
+            position: relative;
+            width: 95%;
+            height: 95vh;
+        }
+
+        .item {
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            width: 125px;
+            height:125px;
+            background-color: red;
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            }
+        }
+        body { 
+            background: linear-gradient(to bottom, #e876bb, #360808);
+            height: 100vh; }
+        .nut {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  background-color: white; 
